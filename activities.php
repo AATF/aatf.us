b44ea63372196bd0b4285f61dd38f098bc5f832d
Felix Wong <felix@fawong.com>
2014-05-16 00:25:54 -0700

    add activities files

---
+++ activities.php
@@ -0,0 +1,35 @@
+<?php
+$title = $current_year . ' Activities';
+include_once('header.php');
+?>
+
+<div class="container">
+<div class="page-header">
+<h1><?php print $title?></h1>
+</div>
+
+<table class="table">
+<?php
+if ($activitieshandle) {
+  while (($buffer = fgets($activitieshandle)) != false) {
+    if (substr(trim($buffer), 0, 1) != "#") {
+      list($date, $event) = explode(' - ', trim($buffer), 2);
+?>
+<tr>
+  <td><?php print $date ?></td>
+  <td><?php print $event ?></td>
+</tr>
+<?php
+    }
+  }
+?>
+</table>
+<?php
+} else {
+?>
+    <p>Coming Soon</p>
+<?php
+}
+?>
+
+<?php include_once('footer.php'); ?>
