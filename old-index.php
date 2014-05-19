046424474cf783cc28559bea67fb01df4d90e8fd
Felix Wong <felix@fawong.com>
2014-05-18 21:42:30 -0700

    image size

--- old-index.php
+++ old-index.php
@@ -111,7 +111,7 @@ if ($activitieshandle) {
 <tr>
 <td>
 <h2><a href="/PosterPolaroids.jpg">Reminisce the Moment</a></h2>
-<a href="/PosterPolaroids.jpg"><img src="PosterPolaroids.jpg" width="350" height="452" alt="Poster" /></a>
+<a href="/PosterPolaroids.jpg"><img src="PosterPolaroids.jpg" width="400" height="517" alt="Poster" /></a>
 <br />
 Date: June 14, 2014 (Saturday)
 <br />
