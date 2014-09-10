d4bdb5f352f44bf13bbb1ddf0a4ba76dd5bffd74
Felix Wong <felix@fawong.com>
2014-09-09 23:35:16 -0700

    add about page

---
+++ about.php
@@ -0,0 +1,40 @@
+<?php
+$title = 'About';
+include_once('header.php');
+?>
+
+<div class="container">
+<div class="page-header">
+<h1><?print $title ?></h1>
+</div>
+
+<img width="142" height="182" src="/images/image002.gif" align="right" hspace="9" alt="image002" />
+<h3>AATF &mdash; Yen Yen Dancing Group</h3>
+
+<h3>Artistic Director and Choreographer:</h3>
+
+<h3>Donna Chen/Ling-Mei Lein</h3>
+
+<p>Yen Yen Dance Group was established by Ms. Donna Chen and Ms. Ling Mei Lien (both of them served as instructors of Yen Yen) because of their affection and enthusiasm to the Chinese Folk Dance.  They brought together a group of Chinese dancers with similar interest to promote the Chinese Culture and dancing arts.  In the last 15 years, Yen Yen had performed in many public events in South California.  In 2003, Yen Yen merged into and became a part of Asian Arts Talents Foundation (AATF), a non-profit PUBLIC BENEFIT CORPORATION organized for public purposes.  The specific purpose of AATF is to raise funds to support Asian arts, education, and culture, to sponsor public arts performance, to offer scholarships for talented young Asian worldwide, to help and get involved with the community during disasters and catastrophes, to organize summer activities for young generation, and to organize dance festivals open to the public.</p>
+
+<p>Instructor Donna Chen was born in Vietnam.  She had learned the Chinese traditional folk dances from many Chinese instructors and was enrolled in the Hai Quan Ballet School when she was a young girl.  In 1975, she joined the City Civil, Industrial and Cultural Dance Group as a formal artist.  In 1976, she was admitted into the Vietnam National Social and Cultural Dance School to receive training to be a certified dance instructor.  Afterwards, she worked as a Choreography Instructor in the Department of Dance at the Tran Kai Quyen School.  Her work had received Distinctive Award in the City of Ho Chi Minh Dance Contest.  Donna came to the U.S. in 1979 and pursued further studies in modern dance.  In 1981, she acted as the Principal Choreographer in the 1st large-scale Chinese public performance organized by the Hai Nan Association at the City Hall of Los Angeles.  In 1986, she started Yen Yen Dancing Group with Instructor Ling-Mei Lein and joined the American Chinese Dance Association.  In 1995 and 2001, Donna Chen was elected the President of the American Chinese Dance Association and she is still now a Director in the Association.  In 2003, she co-founded the Asian Arts Talents Foundation (a non-profit organization) with a few enthusiasts endeavored to promote the Chinese culture.  She now acts as President of the Foundation.</p>
+
+<p>Instructor Ling-Mei Lein was born in Taiwan; she started the dance lesson when she was six.  She was very active in participating in Choreography and frequently represented her school in provincial dance contest in Taiwan and received many awards.  She was an assistant instructor in a famous dance group in Tai Chung, Taiwan.  Since Ms. Lein came to the U. S., she pursued further studies in Jazz and Modern Dance.  When she first met Ms. Donna Chen, who was also fond of Chinese Folk Dance, they formed the Yen Yen Dancing Group in 1986, whereas Instructor Lein was acting as the Art Director of the dancing group.  In 1988, Instructor Lein joined the American Chinese Dance Association and continued her enthusiasm in promoting Chinese Folk Dance and was involved in various charity activities.  In 2003, Instructor Lein co-founded the Asian Arts Talents Foundation (a non-profit organization) with a few enthusiasts endeavored to promote the Asia Arts culture.</p>
+
+<p>Instruction Donna Chen and Ling-Mei Lein both are certified instructors of the Chinese Classification Examination Training Certificate Program of the China Beijing Dancing College.  The curriculum of Yen Yen is designed to teach students in 3 levels, based on their skills: children, teenagers and adults.</p>
+
+<img width="210" height="140" src="/images/image004.gif" align="right" hspace="9" alt="image004" />
+
+
+<p class="text-uppercase text-center">OUR MISSION:</p>
+
+<ul>
+<li>To sponsor public arts performance</li>
+<li>To offer scholarships for talented young Asians worldwide</li>
+<li>To help and get involved with the community during disasters and catastrophes</li>
+<li>To organize summer camps for younger generation</li>
+<li>To organize Dance Festivals &mdash; A dance show open to the public. Proceeds from the event will go to scholarships, grants and/or any other future events.</li>
+<li>To provide Dance Workshops</li>
+</ul>
+
+<?php include_once('footer.php'); ?>
