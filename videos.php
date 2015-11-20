1be6164f84d6a00eebaecaf57f2a52f61f9732d1
Felix Wong <felix@xilef.org>
2015-11-20 00:49:00 -0800

    use https urls for youtube

--- videos.php
+++ videos.php
@@ -8,10 +8,10 @@ include_once('header.php');
 <h1><?php print $title?></h1>
 </div>
 
-<iframe width="560" height="315" src="http://www.youtube.com/embed/-cPpollZLZ4" allowfullscreen></iframe>
-<iframe width="560" height="315" src="http://www.youtube.com/embed/NkLOwsCOvog" allowfullscreen></iframe>
-<iframe width="420" height="315" src="http://www.youtube.com/embed/YEyBkBPFBy0" allowfullscreen></iframe>
-<iframe width="420" height="315" src="http://www.youtube.com/embed/Fvk805y6xQg" allowfullscreen></iframe>
-<iframe width="560" height="315" src="http://www.youtube.com/embed/SWekc9dmp8c" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/-cPpollZLZ4" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/NkLOwsCOvog" allowfullscreen></iframe>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/YEyBkBPFBy0" allowfullscreen></iframe>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/Fvk805y6xQg" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/SWekc9dmp8c" allowfullscreen></iframe>
 
 <?php include_once('footer.php'); ?>
