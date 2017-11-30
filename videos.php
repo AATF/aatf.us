606c983e5810bce03d410251e40cbd140d7426fa
Felix Wong <waf@xilef.org>
2017-11-30 00:43:02 -0800

    add dance festival video

--- videos.php
+++ videos.php
@@ -8,6 +8,10 @@ include_once('header.php');
 <h1><?php print $title?></h1>
 </div>
 
+<p>
+<a href="https://mediazilla.com/tovo51ck9">Video</a>
+</p>
+
 <iframe width="560" height="315" src="https://www.youtube.com/embed/-cPpollZLZ4" allowfullscreen></iframe>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/NkLOwsCOvog" allowfullscreen></iframe>
 <iframe width="420" height="315" src="https://www.youtube.com/embed/YEyBkBPFBy0" allowfullscreen></iframe>
