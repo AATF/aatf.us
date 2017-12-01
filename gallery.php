2f5503844e17dba489de5e4abac534c18fbad9cb
Felix Wong <waf@xilef.org>
2017-12-01 11:31:45 -0800

    initial work on new gallery

--- gallery.php
+++ gallery.php
@@ -9,13 +9,13 @@ include_once('header.php');
 <h1><?php print $title ?></h1>
 </div>
 
-<p><a href="/archives.php">Past Performance Archives</a></p>
-<p><a href="/acdadc.php">Dance Competition Awards</a></p>
-<p><a href="/nyp.php">New Years' Parade</a></p>
-<p><a href="/2005frdf.php">2005 AATF Fundraising Dance Festival</a></p>
-<p><a href="/2007frdf.php">2007 AATF Fundraising Dance Festival</a></p>
-<p><a href="/2009frdf.php">2009 AATF Fundraising Dance Festival</a></p>
-<p><a href="/2010dr.php">2010 AATF Dance Recital</a></p>
-<p><a href="/2013performances.php">2013 AATF Performances</a></p>
+<p><a href="?gallery=archives">Past Performance Archives</a></p>
+<p><a href="?gallery=acdadc">Dance Competition Awards</a></p>
+<p><a href="?gallery=nyp">New Years' Parade</a></p>
+<p><a href="?gallery=2005frdf">2005 AATF Fundraising Dance Festival</a></p>
+<p><a href="?gallery=2007frdf">2007 AATF Fundraising Dance Festival</a></p>
+<p><a href="?gallery=2009frdf">2009 AATF Fundraising Dance Festival</a></p>
+<p><a href="?gallery=2010dr">2010 AATF Dance Recital</a></p>
+<p><a href="?gallery=2013performances">2013 AATF Performances</a></p>
 
 <?php include_once('footer.php'); ?>
