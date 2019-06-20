dee48886016110fa4feb59324fad8b546a34bd1d
Felix Wong <waf@fawong.com>
2019-06-20 20:47:00 +0000

    rename gallery dirs

--- gallery.php
+++ gallery.php
@@ -11,9 +11,9 @@ include_once('header.php');
   <p><a href="?gallery=archives">Past Performance Archives</a></p>
   <p><a href="?gallery=acdadc">Dance Competition Awards</a></p>
   <p><a href="?gallery=nyp">New Years' Parade</a></p>
-  <p><a href="?gallery=2005frdf">2005 AATF Fundraising Dance Festival</a></p>
-  <p><a href="?gallery=2007frdf">2007 AATF Fundraising Dance Festival</a></p>
-  <p><a href="?gallery=2009frdf">2009 AATF Fundraising Dance Festival</a></p>
+  <p><a href="?gallery=2005df">2005 AATF Dance Festival</a></p>
+  <p><a href="?gallery=2007df">2007 AATF Dance Festival</a></p>
+  <p><a href="?gallery=2009df">2009 AATF Dance Festival</a></p>
   <p><a href="?gallery=2010dr">2010 AATF Dance Recital</a></p>
   <p><a href="?gallery=2013performances">2013 AATF Performances</a></p>
 </div>
