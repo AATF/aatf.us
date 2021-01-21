1c6682fbf03ac1c608aab10f1d32637ea7f284b4
Felix Wong <gnowxilef@gmail.com>
2021-01-21 13:59:04 -0800

    update paypal button

--- forms.php
+++ forms.php
@@ -11,12 +11,12 @@ include_once('header.php');
   <h2><a href="/ScholarshipRequirements.pdf" target="_blank">Scholarship Requirements</a></h2>
 
   <h2>Scholarship Application:</h2>
-  <p><a href="/ScholarshipApplication.pdf" target="_blank">PDF Document Format</a></p>
-  <p><a href="/ScholarshipApplication.doc" target="_blank">Word Document Format</a></p>
+  <p><a href="/ScholarshipApplication.pdf" target="_blank">PDF Document</a></p>
+  <p><a href="/ScholarshipApplication.doc" target="_blank">Word Document</a></p>
 
   <h2>Enrollment Form:</h2>
-  <p><a href="/EnrollmentForm.pdf" target="_blank">PDF Document Format</a></p>
-  <p><a href="/EnrollmentForm.doc" target="_blank">Word Document Format</a></p>
+  <p><a href="/EnrollmentForm.pdf" target="_blank">PDF Document</a></p>
+  <p><a href="/EnrollmentForm.doc" target="_blank">Word Document</a></p>
 </div>
 
 <?php include_once('footer.php'); ?>
