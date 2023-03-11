8e47266b8fff87efdcae1643117c978806de0c1f
Felix Wong <felix@waf.hk>
2023-03-10 18:02:31 -0800

    Create

---
+++ noc.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'Network Operations Center';
+include_once('header.php');
+?>
+
+<div class="container">
+  <div class="page-header">
+    <h1><?php print $title ?></h1>
+  </div>
+
+  ARIN and ASN and IPs and such
+</div>
+
+<?php include_once('footer.php'); ?>
