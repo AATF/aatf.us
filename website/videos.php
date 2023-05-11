de6c97397d550816ade029e4a8254cbdac5011e7
Felix Wong <felix@fawong.com>
2023-05-11 00:03:03 +0000

    add youtube link

--- website/videos.php
+++ website/videos.php
@@ -12,11 +12,12 @@ include_once('header.php');
   <p><a href="https://mediazilla.com/tovo51ck9" target="_blank">2017 Biennial Dance Festival</a></p>
 
   <h2>Videos</h2>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/-cPpollZLZ4" allowfullscreen></iframe>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/NkLOwsCOvog" allowfullscreen></iframe>
-  <iframe width="420" height="315" src="https://www.youtube.com/embed/YEyBkBPFBy0" allowfullscreen></iframe>
-  <iframe width="420" height="315" src="https://www.youtube.com/embed/Fvk805y6xQg" allowfullscreen></iframe>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/SWekc9dmp8c" allowfullscreen></iframe>
+  <iframe width="635" height="266" src="https://www.youtube.com/embed/VGzypHhONEI" title="民族風情" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/-cPpollZLZ4" title="Dragon dance" frameborder="0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/NkLOwsCOvog" title="Beautiful Images in the Bamboo Forest" frameborder="0" allowfullscreen></iframe>
+  <iframe width="420" height="315" src="https://www.youtube.com/embed/YEyBkBPFBy0" title="2011 AATF Dance Festival Preview" frameborder="0" allowfullscreen></iframe>
+  <iframe width="420" height="315" src="https://www.youtube.com/embed/Fvk805y6xQg" title="Flying Apsaras" frameborder="0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/SWekc9dmp8c" title="a) Female Shepherd Dance in the Mongolian Prairie and b) Yi Tribe Girl's Sensation" frameborder="0" allowfullscreen></iframe>
 </div>
 
 <?php include_once('footer.php'); ?>
