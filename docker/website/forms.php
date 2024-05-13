3ba48bffeb89aabc75d197d8dd4739b9a8e583c4
Felix Wong <felix@fawong.com>
2024-05-13 07:21:19 +0000

    wow coding

--- docker/website/forms.php
+++ docker/website/forms.php
@@ -13,13 +13,13 @@ $links = [
   "Scholarship Requirements" => "ScholarshipRequirements.pdf",
   "Scholarship Application" => "ScholarshipApplication.pdf",
   "Enrollment Form" => "EnrollmentForm.pdf",
-  "Adult class registration" => "AATF_AACC_Registration_Form_ADULT_CLASS_v2024_05.pdf",
-  "weekend adult class registration" => "AATF_AACC_Registration_Form_SATURDAY_v2024_05.pdf",
-  "summer camp registration" => "AATF_AACC_Registration_Form_SUMMER_CAMP_v2024_05.pdf",
+  "Adult class registration" => "summer-school/AATF_AACC_Registration_Form_ADULT_CLASS_v2024_05.pdf",
+  "weekend adult class registration" => "summer-school/AATF_AACC_Registration_Form_SATURDAY_v2024_05.pdf",
+  "summer camp registration" => "summer-school/AATF_AACC_Registration_Form_SUMMER_CAMP_v2024_05.pdf",
 ];
 foreach($links as $text => $url) {
 ?>
-  <p><a href="//<?php print $cdn_url; ?>/<?php print $link ?>" target="_blank"><?php print $text ?></a></p>
+  <p><a href="//<?php print $cdn_url; ?>/<?php print $url ?>" target="_blank"><?php print $text ?></a></p>
 <?php
 }
 ?>
