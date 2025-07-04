3ec321611b9a7a8c1ef4da2547a295aac9a2f3a1
Felix Wong <felix@waf.hk>
2025-07-04 03:16:00 -0700

    Create acknowledgments.php
    Signed-off-by: Felix Wong <felix@waf.hk>

---
+++ website/acknowledgments.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'Acknowledgments';
+include_once('header.php');
+?>
+
+<div class="container">
+  <div class="page-header">
+    <h1><?php print $title ?></h1>
+  </div>
+
+  <p>Juan Soberanes - http://www.cyber-ducky.com</p>
+</div>
+
+<?php include_once('footer.php'); ?>
