e159fef5ef1603a563cda3b81c1c499a89fe4189
felix wong <felix.wong@waf.hk>
2026-06-15 00:11:05 -0700

    kinda

--- scripts/deploy.go
+++ scripts/deploy.go
@@ -4,13 +4,24 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
+	"runtime"
 	"strings"
 )
 
 func main() {
-	content, err := os.ReadFile("../docker/VERSION")
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		slog.Error("failed to get caller information")
+		os.Exit(1)
+	}
+	dir := filepath.Dir(filename)
+	versionPath := filepath.Join(dir, "..", "docker", "VERSION")
+
+	content, err := os.ReadFile(versionPath)
 	if err != nil {
 		slog.Error("failed to read version file", "error", err)
+		os.Exit(1)
 	}
 	version := strings.TrimSpace(string(content))
 
